b142cfba4cc55b6211dcbbfdb51719daeee65ddd
achesak <achesak@yahoo.com>
2013-10-17 17:06:23 -0500

    Finished types.

--- coverartarchive.nim
+++ coverartarchive.nim
@@ -6,3 +6,12 @@
 # Import modules.
 import httpclient
 import json
+
+
+# Define the types.
+type TCoverArtData* = tuple[images : seq[TCoverArtImage], release : string]
+
+type TCoverArtImage* = tuple[types : seq[string], front : bool, back : bool, edit : int, image : string, comment : string,
+                             approved : bool, thumbnails : TCoverArtThumbnails, id : string]
+
+type TCoverArtThumbnails* = tuple[large : string, small : string]
